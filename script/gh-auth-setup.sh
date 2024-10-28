35f27354d1239313cbc19535e78fc7c492d30f74
gael-herrera <gael.herrera@uabc.edu.mx>
2024-10-27 22:06:00 -0700

    Added Github SSH auth setup script for repository
    This script allows to setup an SSH key unique for this repository. Which
    is useful for me since I have other account in my local computer.

---
+++ script/gh-auth-setup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+dir="key"
+
+key_filename="github"
+config_filename="github-ssh.config"
+
+config_content="Host github.com\n\tHostName github.com\n\tPort 22\n\tUser git\n\tIdentityFile $dir/$key_filename"
+
+printf "[INPUT]: Type your GitHub-linked email address: "
+read -r email
+
+printf "[INPUT]: Type passphrase for the key (leave empty for no passphrase): "
+read -sr passphrase
+echo
+
+git_config_ssh_cmd=$(git config core.sshCommand)
+
+if [[ -f "$dir/$key_filename" || -f "$dir/$key_filename.pub" ]]; then
+    echo
+    echo "[WARN]: SSH key files already exist at '$dir/$key_filename' or '$dir/$key_filename.pub'."
+    printf "[INPUT]: Type 'y' to continue anyway; type any other key to exit: "
+    read should_continue
+
+    if [[ "$should_continue" != "y" ]]; then
+        echo "[INFO]: Github SSH auth setup canceled."
+        exit 0
+    fi
+
+    rm "$dir/$key_filename" "$dir/$key_filename.pub"
+fi
+
+if grep -q "Host github.com" "$dir/$config_filename" >/dev/null 2>&1; then
+    echo "[WARN]: GitHub SSH configuration already exists in $dir/$config_filename."
+    printf "[INPUT]: Type 'y' to continue anyway; type any other key to exit: "
+    read should_continue
+
+    if [[ "$should_continue" != "y" ]]; then
+        echo "[INFO]: Github SSH auth setup canceled."
+        exit 0
+    fi
+
+    rm "$dir/$config_filename"
+fi
+
+if [[ "$git_config_ssh_cmd" != "ssh" && -n "$git_config_ssh_cmd" ]]; then
+    echo
+    echo "[WARN]: Git 'git config core.sshCommand' is set to '$git_config_ssh_cmd' modifying this could cause issues."
+    printf "[INPUT]: Type 'y' to continue anyway; type any other key to exit: "
+    read should_continue
+
+    if [[ "$should_continue" != "y" ]]; then
+        echo "[INFO]: Github SSH auth setup canceled."
+        exit 0
+    fi
+fi
+
+mkdir -p "$dir"
+ssh-keygen -t ed25519 -C "$email" -f "$dir/$key_filename" -N "$passphrase" >/dev/null 2>&1
+echo -e "$config_content" >>"$dir/$config_filename"
+git config core.sshCommand "ssh -F $dir/$config_filename"
+
+echo
+echo "[INFO]: SSH key generated and Git configured to use it."
+echo "[INFO]: Please add the following public SSH key on https://github.com/settings/keys: '$(cat $dir/$key_filename.pub)'"
